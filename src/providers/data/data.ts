553736cf0a6c55d0aae9cf34e4296a0ba97288a5
essejose <ze_pk2@hotmail.com>
2018-05-04 18:51:07 -0300

    init

---
+++ src/providers/data/data.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+ 
+@Injectable()
+export class DataProvider {
+  
+  data: any;
+  dataResult:any;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello DataProvider Provider');
+  }
+
+  load(){
+    
+           if(this.data){
+               return Promise.resolve(this.data);
+           } 
+           return new Promise(resolve => {
+    
+               this.http.get('assets/data/questions.json').subscribe(result => {
+                   this.data = result;
+                   resolve(this.data.questions);
+               });
+    
+           });
+    
+  }
+
+} 
